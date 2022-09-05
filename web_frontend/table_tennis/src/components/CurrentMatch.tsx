acb1f42036f7f153c4d674652a372b649162a460
Polina Simonenko <rabarbrablad@gmail.com>
2022-09-05 11:24:40 +0400

    feat: added current match component

--- web_frontend/table_tennis/src/components/CurrentMatch.tsx
+++ web_frontend/table_tennis/src/components/CurrentMatch.tsx
@@ -1,6 +1,7 @@
 import Card                 from '@mui/material/Card';
 import CardContent          from '@mui/material/CardContent';
 import Box                  from '@mui/material/Box';
+import Typography           from '@mui/material/Typography';
 import Match                from 'interfaces/Match';
 import Player               from 'interfaces/Player';
 import { running_matches }  from 'interfaces/test_data';
@@ -24,11 +25,14 @@ const PlayerCard = (
 const CurrentMatch = () => {
     const data: Match = running_matches[0];
     return(
-        <Box sx={{display: 'flex'}}>
+        <Box sx={{display: 'flex', alignItems: 'baseline'}}>
             <PlayerCard
                 player={data.a_player}
                 goals={data.goals_a || 0}
             />
+            <Typography variant='h4'>
+                vs
+            </Typography>
             <PlayerCard
                 player={data.b_player}
                 goals={data.goals_b || 0}
