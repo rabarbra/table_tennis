6149015602d1f6365807de26b553cbc8b4aed179
Polina Simonenko <rabarbrablad@gmail.com>
2022-09-19 00:00:08 +0400

    fix: fixed profile card positioning

--- web_frontend/table_tennis/src/pages/Profile.tsx
+++ web_frontend/table_tennis/src/pages/Profile.tsx
@@ -3,6 +3,7 @@ import Card                 from "@mui/material/Card";
 import CardContent          from '@mui/material/CardContent';
 import CardMedia            from "@mui/material/CardMedia";
 import Typography           from "@mui/material/Typography";
+import { TOPBAR_HEIGHT } from "config/config";
 import * as Avatar          from "images/avatars";
 import Player               from 'interfaces/Player';
 import { players }          from 'interfaces/test_data';
@@ -16,13 +17,14 @@ const Profile = () => {
                 height: '100%',
                 mx: 'auto',
                 display: 'flex',
-                justifyContent: 'center'
+                justifyContent: 'center',
             }}
         >
             <Card
                 sx={{
                     margin: 'auto',
                     padding: 2,
+                    transform: `translateY(-${TOPBAR_HEIGHT / 2}px)`
                 }}
             >
                 <CardMedia
